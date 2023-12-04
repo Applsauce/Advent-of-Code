bace63d5b9fe6e22e534f9148919566ba29dd8e9
Fredrik Österberg <fredrios@axis.com>
2023-12-04 13:19:40 +0100

    day 2 prep
    Change-Id: If1def850dff1466e7c2bc0346d1e199a7b2896f3

---
+++ 2023/day2.py
@@ -0,0 +1,21 @@
+from aocd.models import Puzzle
+
+YEAR = 2023
+DAY = 2
+
+puzzle = Puzzle(year=YEAR, day=DAY)
+
+
+print(puzzle.examples[0].input_data)
+print(puzzle.examples[0].answer_a)
+
+
+def a():
+    print(puzzle.examples)
+
+
+
+def b():
+    test = 0
+
+a()
