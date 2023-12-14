20c61844675d4ddac27ba74eebb8009c2c4f4da2
Fredrik Österberg <fredrios@axis.com>
2023-12-14 10:20:38 +0100

    day 2
    Change-Id: Icdf91a7b1ca1a73c1057eb90902bc1077a0f369e

--- 2023/day2.py
+++ 2023/day2.py
@@ -1,4 +1,5 @@
 from aocd.models import Puzzle
+import re
 
 YEAR = 2023
 DAY = 2
@@ -6,16 +7,84 @@ DAY = 2
 puzzle = Puzzle(year=YEAR, day=DAY)
 
 
-print(puzzle.examples[0].input_data)
-print(puzzle.examples[0].answer_a)
-
-
 def a():
-    print(puzzle.examples)
+    # print(puzzle.examples[0].input_data)
+    # print(puzzle.examples[0].answer_a)
+
+    # 12 r, 13g, 14b
+    red_max = 12
+    green_max = 13
+    blue_max = 14
+    id_tot = 0
+    for line in puzzle.input_data.splitlines():
+        id = line.split(":")[0]
+        id = int(re.search(r"\d+", id).group())
+
+        game = line.split(":")[1]
+        game = game.replace(";", ",")
+        game = game.split(",")
+        red_largest = 0
+        green_largest = 0
+        blue_largest = 0
+
+        for gline in game:
+            if "red" in gline:
+                red = int(re.search(r"\d+", gline).group())
+                if red > red_largest:
+                    red_largest = red
+            elif "green" in gline:
+                green = int(re.search(r"\d+", gline).group())
+                if green > green_largest:
+                    green_largest = green
+            elif "blue" in gline:
+                blue = int(re.search(r"\d+", gline).group())
+                if blue > blue_largest:
+                    blue_largest = blue
+
+        print(id, red_largest, green_largest, blue_largest)
+        if (
+            (red_largest <= red_max)
+            and (green_largest <= green_max)
+            and (blue_largest <= blue_max)
+        ):
+            id_tot += id
+            print(id_tot)
 
 
-
+# Minimum cubes present
 def b():
-    test = 0
+    print(puzzle.examples[0].input_data)
 
-a()
+    id_tot = 0
+    for line in puzzle.input_data.splitlines():
+        id = line.split(":")[0]
+        id = int(re.search(r"\d+", id).group())
+
+        game = line.split(":")[1]
+        game = game.replace(";", ",")
+        game = game.split(",")
+        red_largest = 0
+        green_largest = 0
+        blue_largest = 0
+
+        for gline in game:
+            if "red" in gline:
+                red = int(re.search(r"\d+", gline).group())
+                if red > red_largest:
+                    red_largest = red
+            elif "green" in gline:
+                green = int(re.search(r"\d+", gline).group())
+                if green > green_largest:
+                    green_largest = green
+            elif "blue" in gline:
+                blue = int(re.search(r"\d+", gline).group())
+                if blue > blue_largest:
+                    blue_largest = blue
+
+        smallest_cube = red_largest * green_largest * blue_largest
+        print(smallest_cube, red_largest, green_largest, blue_largest)
+        id_tot += smallest_cube
+        print(id_tot)
+
+
+b()
